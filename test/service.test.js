8074ee1aee395f51f6b8398d80d727fe941eec34
davidlim7223 <david98@vt.edu>
2022-08-03 21:50:53 -0400

    added testSuite

---
+++ test/service.test.js
@@ -0,0 +1,8 @@
+describe('Checks for the test suite', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  })
+  it('should also pass', () => {
+    expect(1).toBe(1);
+  })
+});
